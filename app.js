691f978c1618eb8a3f7bace91f96348f00ff0ab4
Samuel <samuelsoutosilva14@gmail.com>
2022-06-29 14:31:43 -0300

    [ initial commit ]

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const app = express();
+const app = express(); 
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
