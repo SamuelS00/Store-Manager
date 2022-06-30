85d494176cdd9dd34505ea32e15faf723adcb8ed
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 11:50:57 -0300

    fix: remove double quotes

--- models/productModels.js
+++ models/productModels.js
@@ -1,14 +1,14 @@
-const connection = require("./connection");
+const connection = require('./connection');
 
 const getAll = async () => {
-  const query = "SELECT * FROM StoreManager.products";
+  const query = 'SELECT * FROM StoreManager.products';
   const [products] = await connection.execute(query);
 
   return products;
 };
 
 const getById = async (id) => {
-  const query = "SELECT * FROM StoreManager.products WHERE id = ?";
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
   const [product] = await connection.execute(query, [id]);
 
   return product[0];
