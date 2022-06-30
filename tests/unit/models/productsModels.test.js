7500f23a85e8e065750ab94ab152654029c7be4a
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 13:29:29 -0300

    test: getbyid method in the model layer

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -4,33 +4,61 @@ const { expect } = require('chai');
 const connection = require('../../../models/connection');
 const ProductModel = require('../../../models/productModels');
 
-const { mockProductsGetAll } = require('../mocks/modelsMocks');
+const { mockProductsGetAll, mockProductGetById } = require('../mocks/modelsMocks');
 
 describe('( model layer )', () => {
   describe('#method getAll', () => {
     before(async () => {
       const query = [mockProductsGetAll];
-      sinon.stub(connection, "execute").resolves(query);
+      sinon.stub(connection, 'execute').resolves(query);
     });
 
     after(async () => {
       connection.execute.restore();
     });
 
-    describe("When the product is successfully created", () => {
-      it("tests if the return is an object", async () => {
+    describe('when all products are returned', () => {
+      it('tests if the return is an object', async () => {
         const response = await ProductModel.getAll();
-        expect(response[0]).to.be.a("object");
+        expect(response[0]).to.be.a('object');
       });
 
-      it("tests if o return has the name of the inserted product", async () => {
+      it('tests if o return has the name of the inserted product', async () => {
         const response = await ProductModel.getAll();
-        expect(response[0]).to.be.a.property("name");
+        expect(response[0]).to.be.a.property('name');
       });
 
-      it("tests if o return has the id of the inserted product", async () => {
+      it('tests if o return has the id of the inserted product', async () => {
         const response = await ProductModel.getAll();
-        expect(response[0]).to.be.a.property("id");
+        expect(response[0]).to.be.a.property('id');
+      });
+    });
+  });
+
+  describe('#method getById', () => {
+    before(async () => {
+      const query = [mockProductGetById];
+      sinon.stub(connection, 'execute').resolves(query);
+    });
+
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    describe('when only products by id are returned', () => {
+      it('tests if the return is an object', async () => {
+        const response = await ProductModel.getById(3);
+        expect(response[0]).to.be.a('object');
+      });
+
+      it('tests if o return has the name of the inserted product', async () => {
+        const response = await ProductModel.getById(3);
+        expect(response[0]).to.be.a.property('name');
+      });
+
+      it('tests if o return has the id of the inserted product', async () => {
+        const response = await ProductModel.getById(3);
+        expect(response[0]).to.be.a.property('id');
       });
     });
   });
