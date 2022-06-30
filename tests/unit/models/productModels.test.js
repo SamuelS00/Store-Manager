7b9bc84623b8483497e14a2266797a7111fe8e26
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 16:19:21 -0300

    test: refactor model layer tests

--- tests/unit/models/productModels.test.js
+++ tests/unit/models/productModels.test.js
@@ -6,20 +6,20 @@ const ProductModel = require('../../../models/productModels');
 
 const { mockProductsGetAll, mockProductGetById } = require('../mocks/mocksProducts');
 
-describe('( model layer )', () => {
+describe.only('( model layer )', () => {
   describe('#method getAll', () => {
-    before(async () => {
-      const query = [mockProductsGetAll];
-      sinon.stub(connection, 'execute').resolves(query);
-    });
-
-    after(async () => {
-      connection.execute.restore();
-    });
-
     describe('when all products are returned', () => {
+      before(async () => {
+        const query = [mockProductsGetAll];
+        sinon.stub(connection, "execute").resolves(query);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
+
       it('tests if the return is an object', async () => {
-        const response = await ProductModel.getAll();
+        const response = await ProductModel.getAll(); // arrumar.
         expect(response[0]).to.be.a('object');
       });
 
@@ -36,16 +36,15 @@ describe('( model layer )', () => {
   });
 
   describe('#method getById', () => {
-    before(async () => {
-      const query = [mockProductGetById];
-      sinon.stub(connection, 'execute').resolves(query);
-    });
-
-    after(async () => {
-      connection.execute.restore();
-    });
-
     describe('when only products by id are returned', () => {
+      before(async () => {
+        const query = [mockProductGetById];
+        sinon.stub(connection, 'execute').resolves(query);
+      });
+
+      after(async () => {
+        connection.execute.restore();
+      });
       it('tests if the return is an object', async () => {
         const response = await ProductModel.getById(3);
         expect(response).to.be.a('object');
