59f5e56d3800853c40ed6570e25a7325e9e14307
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 16:18:38 -0300

    fix: fix import

--- services/productServices.js
+++ services/productServices.js
@@ -1,9 +1,8 @@
 const ProductModel = require('../models/productModels');
-const validateId = require('../helpers/validateId');
+const { validateId } = require('../helpers/validateId');
 
 const getAll = async () => {
   const products = await ProductModel.getAll();
-
   return products;
 };
 
@@ -11,7 +10,6 @@ const getById = async (id) => {
   await validateId(id);
 
   const product = await ProductModel.getById(id);
-
   return product;
 };
 
