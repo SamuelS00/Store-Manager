46fd6fa8acf3dd9c95d86f68e13919160a06961b
Samuel <samuelsoutosilva14@gmail.com>
2022-06-29 22:50:04 -0300

    feat: add services layer

---
+++ services/productServices.js
@@ -0,0 +1,19 @@
+const ProductModel = require('../models/productModels');
+const validateId = require('../helpers/validateId');
+const notFound = require('../errors/notFound');
+
+const getAll = async () => {
+  const products = await ProductModel.getAll();
+
+  return products;
+};
+
+const getById = async (id) => {
+  await validateId(id);
+
+  const product = await ProductModel.getById(id);
+
+  return product;
+};
+
+module.exports = { getAll, getById };
