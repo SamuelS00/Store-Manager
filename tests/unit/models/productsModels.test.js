6366fc8eb0feac92118f51458e45bf9b3ca97208
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 13:32:00 -0300

    test: test getbyid refactor

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -48,17 +48,17 @@ describe('( model layer )', () => {
     describe('when only products by id are returned', () => {
       it('tests if the return is an object', async () => {
         const response = await ProductModel.getById(3);
-        expect(response[0]).to.be.a('object');
+        expect(response).to.be.a('object');
       });
 
       it('tests if o return has the name of the inserted product', async () => {
         const response = await ProductModel.getById(3);
-        expect(response[0]).to.be.a.property('name');
+        expect(response).to.be.a.property('name');
       });
 
       it('tests if o return has the id of the inserted product', async () => {
         const response = await ProductModel.getById(3);
-        expect(response[0]).to.be.a.property('id');
+        expect(response).to.be.a.property('id');
       });
     });
   });
