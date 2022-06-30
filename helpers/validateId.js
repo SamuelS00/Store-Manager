4955822c5e06d1bd5a973142cd6b7eb115c7c63d
Samuel <samuelsoutosilva14@gmail.com>
2022-06-29 22:49:49 -0300

    feat: add id validation

---
+++ helpers/validateId.js
@@ -0,0 +1,11 @@
+const ProductModel = require('../models/productModels');
+const notFound = require('../errors/notFound');
+
+const validateId = async (id) => {
+  const products = await ProductModel.getAll();
+  const productById = products.find((p) => p.id === Number(id));
+
+  if (!productById) throw notFound('Product not found');
+};
+
+module.exports = validateId;
