194f07bffc31fbeff7a0f51030b37eead2022a08
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 16:58:49 -0300

    feat: add body-parser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
-const app = express(); 
+const bodyParser = require('body-parser');
 const ProductRouter = require('./routes/productRouter');
 const ErrorMiddleware = require('./middlewares/ErrorMiddleware');
+
+const app = express(); 
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/products', ProductRouter);
 
