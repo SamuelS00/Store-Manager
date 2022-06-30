9b72f8870da74b986973699840b7e5a3ef0ddf5d
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 11:51:33 -0300

    fix: ignore the next parameter

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,13 +1,13 @@
 const ProductService = require('../services/productServices');
 const httpsStatusCode = require('../helpers/httpStatusCode');
 
-const getAll = async (_req, res, next) => {
+const getAll = async (_req, res, _next) => {
   const products = await ProductService.getAll();
 
   res.status(httpsStatusCode.OK).json(products);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res, _next) => {
   const { id } = req.params;
   const product = await ProductService.getById(id);
 
