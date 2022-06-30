1f2565225fd64a2d39290f994fb8dd1b59a0e50b
Samuel <samuelsoutosilva14@gmail.com>
2022-06-29 22:45:15 -0300

    feat: add a route product in the app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express(); 
+const ProductRouter = require('./routes/productRouter');
+
+app.use('/products', ProductRouter);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
