c73ae46ff8e8dd942c6483c4e6a3a9dda49daf99
Samuel <samuelsoutosilva14@gmail.com>
2022-06-30 11:54:18 -0300

    fix: fix: import order and remove double quotes

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+const rescue = require('express-rescue');
 const ProductController = require('../controllers/productControllers');
-const rescue = require("express-rescue");
+
 const router = express.Router();
 
-router.get("/", rescue(ProductController.getAll));
+router.get('/', rescue(ProductController.getAll));
 
-router.get("/:id", rescue(ProductController.getById));
+router.get('/:id', rescue(ProductController.getById));
 
 module.exports = router;
