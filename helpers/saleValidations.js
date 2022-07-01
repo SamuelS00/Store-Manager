6f282c255fde944d5a1a462ac33b6f879c95d398
Samuel <samuelsoutosilva14@gmail.com>
2022-07-01 16:39:44 -0300

    feat: add validation in service layer

---
+++ helpers/saleValidations.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+
+const badRequest = require('../errors/badRequest');
+const unprocessableEntity = require('../errors/unprocessableEntity');
+const joiTypeError = require('./joiTypeError');
+
+const { validateId } = require('./productValidations');
+
+const saleSchema = Joi.object({
+  productId: Joi.number().required(),
+  quantity: Joi.number().min(1).required(),
+});
+
+const validateSale = async ({ productId, quantity }) => {
+  const { error } = saleSchema.validate({ productId, quantity });
+
+  if (error) {
+    const {
+      details: [{ type }],
+    } = error;
+
+    if (joiTypeError.TYPE_ANY_REQUIRED === type) throw badRequest(error.message);
+    if (joiTypeError.TYPE_NUMBER_MIN === type) throw unprocessableEntity(error.message);
+  }
+
+  await validateId(productId);
+};
+
+module.exports = { validateSale };
