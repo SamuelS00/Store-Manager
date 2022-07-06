81c4fa5f0bc9cde1d84325b47bfab97b179bbdca
Samuel <samuelsoutosilva14@gmail.com>
2022-07-06 14:14:42 -0300

    fix: ..........................................ahh

--- models/saleModels.js
+++ models/saleModels.js
@@ -2,41 +2,36 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const query = `
-    SELECT sp.sale_id, s.date, sp.product_id, sp.quantity FROM StoreManager.sales AS s
+    SELECT 
+      sp.sale_id AS saleId,
+      s.date, sp.product_id AS productId,
+      sp.quantity 
+    FROM StoreManager.sales AS s
     INNER JOIN StoreManager.sales_products AS sp
     ON s.id = sp.sale_id
     ORDER BY sp.sale_id, sp.product_id ASC`;
   
-  const [result] = await connection.execute(query);
-
-  const sales = result.map((sale) => {
-    const SALE_ID = 'saleId';
-    const PRODUCT_ID = 'productId';
-    delete Object.assign(sale, { [SALE_ID]: sale.sale_id }).sale_id;
-    delete Object.assign(sale, { [PRODUCT_ID]: sale.product_id }).product_id;
-    return sale;
-  });
+  const [sales] = await connection.execute(query);
 
   return sales;
 };
 
 const getById = async (id) => {
   const query = `
-    SELECT sp.sale_id, s.date, sp.product_id, sp.quantity FROM StoreManager.sales AS s
+    SELECT 
+      sp.sale_id, 
+      s.date, 
+      sp.product_id AS productId,
+      sp.quantity 
+    FROM StoreManager.sales AS s
     INNER JOIN StoreManager.sales_products AS sp
     ON s.id = sp.sale_id
     WHERE sp.sale_id = ? 
     ORDER BY sp.sale_id, sp.product_id ASC`;
   
-  const [result] = await connection.execute(query, [id]);
+  const [sale] = await connection.execute(query, [id]);
 
-  const sales = result.map((sale) => {
-    const PRODUCT_ID = 'productId';
-    delete Object.assign(sale, { [PRODUCT_ID]: sale.product_id }).product_id;
-    return sale;
-  });
-
-  return sales;
+  return sale;
 };
 
 const createSale = async () => {
